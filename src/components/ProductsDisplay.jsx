31b76afc641892cc6d345279ab9f451cf03e4c88
Anthony-0801 <zyxanthony15@gmail.com>
2025-02-07 13:13:02 +0800

    finished displaying the specific recipe for a certain dish

--- src/components/ProductsDisplay.jsx
+++ src/components/ProductsDisplay.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
 
 export const ProductsDisplay = ({ food }) => {
-  const [item, setItem] = useState(["656329"]);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [recipe, setRecipe] = useState(null);
 
   useEffect(() => {
-    async function setRecipe() {
-    const res = await fetch(`${import.meta.env.VITE_API_RECIPE}${item}/information?apiKey=${import.meta.env.VITE_API_KEY}`);
-    const data = await res.json();
-    console.log(data);
+    async function setRecipes() {
+      if (selectedItem) {
+        const res = await fetch(
+          `${import.meta.env.VITE_API_RECIPE}${selectedItem}/information?apiKey=${import.meta.env.VITE_API_KEY}`
+        );
+        const data = await res.json();
+        setRecipe(data);
+      }
     }
-    setRecipe();
-}, [item])
+    setRecipes();
+  }, [selectedItem]);
 
   return (
     <section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
@@ -20,8 +25,7 @@ export const ProductsDisplay = ({ food }) => {
             Food for Thought
           </h2>
           <p className="max-w-lg mx-auto mt-4 text-base leading-relaxed text-gray-600 italic">
-            "One cannot think well, love well, sleep well, if one has not dined
-            well." - Virginia Woolf
+            "One cannot think well, love well, sleep well, if one has not dined well." - Virginia Woolf
           </p>
         </div>
 
@@ -36,7 +40,7 @@ export const ProductsDisplay = ({ food }) => {
                   <label
                     className="px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase bg-green-700 rounded-md drawer-button"
                     htmlFor="my-drawer-4"
-                    onClick={() => setItem(item.id)}
+                    onClick={() => setSelectedItem(item.id)}
                   >
                     View Recipe
                   </label>
@@ -52,10 +56,10 @@ export const ProductsDisplay = ({ food }) => {
                     className="w-full lg:hidden bg-green-700 text-white p-2 rounded-md"
                     onClick={() => {
                       document.getElementById("my_modal_3").showModal();
-                      setItem(item.id);
+                      setSelectedItem(item.id);
                     }}
                   >
-                    Show Recipe
+                    View Recipe
                   </button>
                 </div>
                 <blockquote className="mt-5">
@@ -76,8 +80,33 @@ export const ProductsDisplay = ({ food }) => {
                   ✕
                 </button>
               </form>
-              <h3 className="font-bold text-lg">Recipe for {item.title}</h3>
-              <p className="py-4">{item.id}</p>
+
+                {/* Modal content here */}
+                <img className="w-full mb-8 rounded-md" src={recipe?.image} alt={recipe?.title} />
+                <h3 className=" font-bold mb-2 text-lg">Recipe for {recipe?.title}</h3>
+                <h4 className="font-bold mb-2">Instructions</h4>
+                {recipe?.analyzedInstructions[0].steps.map((step) => (
+                  <div key={step.number} className="mb-6">
+                    <p><span className="font-semibold mb-2">{step.number}. </span> 
+                    {step.step}</p>
+                  </div>
+                ))}
+
+                <ul className="mb-8">
+                  <h4 className="font-bold mb-2">Ingredients</h4>
+                  {recipe?.extendedIngredients.map((ingredient) => (
+                    <li key={ingredient.id} className="mb-2 list-disc ml-4">
+                      {ingredient.original}
+                    </li>
+                  ))}
+                  <h4 className="font-bold mb-2">Additional Information</h4>
+                  <li>Preparation Time: {recipe?.readyInMinutes} minutes</li>
+                  <li>Servings: {recipe?.servings}</li>
+                  <li>Dairy-free: {recipe?.dairyFree ? "🐮 Yes" : "❌ No "}</li>
+                  <li>Gluten-free: {recipe?.glutenFree ? "🌾 Yes" : "❌ No "}</li>
+                </ul>
+
+              
             </div>
           </dialog>
 
@@ -91,16 +120,33 @@ export const ProductsDisplay = ({ food }) => {
                 aria-label="close sidebar"
                 className="drawer-overlay"
               ></label>
-              <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+              <div className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                 {/* Sidebar content here */}
-                <h3>Recipe for {item.title}</h3>
-                <li>
-                  <a>{item}</a>
-                </li>
-                <li>
-                  <a>Sidebar Item 2</a>
-                </li>
-              </ul>
+                <img className="w-full mb-8 rounded-md" src={recipe?.image} alt={recipe?.title} />
+                <h3 className=" font-bold mb-2 text-lg">Recipe for {recipe?.title}</h3>
+                <h4 className="font-bold mb-2">Instructions</h4>
+                {recipe?.analyzedInstructions[0].steps.map((step) => (
+                  <div key={step.number} className="mb-6">
+                    <p><span className="font-semibold mb-2">{step.number}. </span> 
+                    {step.step}</p>
+                  </div>
+                ))}
+
+                <ul className="mb-8">
+                  <h4 className="font-bold mb-2">Ingredients</h4>
+                  {recipe?.extendedIngredients.map((ingredient) => (
+                    <li key={ingredient.id} className="mb-2 list-disc ml-4">
+                      {ingredient.original}
+                    </li>
+                  ))}
+                  <h4 className="font-bold mb-2">Additional Information</h4>
+                  <li>Preparation Time: {recipe?.readyInMinutes} minutes</li>
+                  <li>Servings: {recipe?.servings}</li>
+                  <li>Dairy-free: {recipe?.dairyFree ? "🐮 Yes" : "❌ No "}</li>
+                  <li>Gluten-free: {recipe?.glutenFree ? "🌾 Yes" : "❌ No "}</li>
+                </ul>
+
+              </div>
             </div>
           </div>
         </div>
